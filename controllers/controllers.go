67dc89f40933be41baaa7b5008f1877ed22e209a
Rishav Naskar <rishavnaskar.r@gmail.com>
2021-10-09 23:52:31 +0530

    feat: final updates

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const connectionString = "mongodb+srv://admin:R1i2s3hav@cluster0.nodr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -89,6 +90,50 @@ func getOnePost(postId string) {
 	fmt.Println("Post details ", result)
 }
 
+//Get all users
+func getAllUsers() []primitive.M {
+	cur, err := collection1.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var users []primitive.M
+
+	for cur.Next(context.Background()) {
+		var user bson.M
+		err := cur.Decode(&user)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	defer cur.Close(context.Background())
+	return users
+}
+
+//Get all posts
+func getAllPosts() []primitive.M {
+	cur, err := collection2.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var posts []primitive.M
+
+	for cur.Next(context.Background()) {
+		var post bson.M
+		err := cur.Decode(&post)
+		if err != nil {
+			log.Fatal(err)
+		}
+		posts = append(posts, post)
+	}
+
+	defer cur.Close(context.Background())
+	return posts
+}
+
 //Controllers - files
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -97,6 +142,8 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	var user models.User
 	_ = json.NewDecoder(r.Body).Decode(&user)
+	res := hashAndSalt(user.Password)
+	user.Password = res
 	insertOneUser(user)
 	json.NewEncoder(w).Encode(user)
 }
@@ -112,7 +159,7 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
 	w.Header().Set("Allow-Control-Allow-Methods", "GET")
 
 	params := mux.Vars(r)
@@ -127,4 +174,26 @@ func GetAPost(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	getOnePost(params["id"])
 	json.NewEncoder(w).Encode(params["id"])
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	allUsers := getAllUsers()
+	json.NewEncoder(w).Encode(allUsers)
+}
+
+func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	allPosts := getAllPosts()
+	json.NewEncoder(w).Encode(allPosts)
+}
+
+func hashAndSalt(pwd string) string {
+	b := []byte(pwd)
+	hash, err := bcrypt.GenerateFromPassword(b, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("")
+	return string(hash)
 }
--- routers/routers.go
+++ routers/routers.go
@@ -5,13 +5,16 @@ import (
 	"github.com/rishavnaskar/insta/controllers"
 )
 
-func Router()  *mux.Router{
+func Router() *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users",controllers.CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}",controllers.GetAUser).Methods("GET")
-	r.HandleFunc("/posts",controllers.CreatePost).Methods("POST")
-	r.HandleFunc("/posts/{id}",controllers.GetAPost).Methods("POST")
+	r.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
+	r.HandleFunc("/users", controllers.CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id}", controllers.GetAUser).Methods("GET")
+
+	r.HandleFunc("/posts", controllers.GetAllPosts).Methods("GET")
+	r.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
+	r.HandleFunc("/posts/{id}", controllers.GetAPost).Methods("GET")
 
 	return r
 }
